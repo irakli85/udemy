4860e51446eae58ee4c3872e0a9413662eb3235a
ika <iberidze38@gmail.com>
2022-10-19 00:16:52 +0400

    first excercise

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let incr = 10,
-    decr = 10;
+let numberOfFilms = prompt('how many films have you watched?', '');
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
 
-console.log(--incr);
-console.log(++decr);
-console.log(++decr);
+let question1 = prompt('last movie you watched?','');
+let question2 = prompt('rate this movie','');
+let question3 = prompt('last movie you watched?','');
+let question4 = prompt('rate this movie','');
+
+personalMovieDB.movies[question1] = question2;
+personalMovieDB.movies[question3] = question4;
+
+console.log(personalMovieDB);
+
